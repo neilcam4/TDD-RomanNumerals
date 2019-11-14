4d5279df1be8c554f8e566bf8af47e3393308e10
Neil Campbell <neildcampbell@hotmail.com>
2019-11-14 05:52:28 +0000

    added test for I

---
+++ spec/romanNumeralsSpec.js
@@ -0,0 +1,7 @@
+describe("ROMAN NUMERALS", function(){
+    it("should return 1 with i", function(){
+        let num = 'I'
+        let result = romanNumConvert(num)
+        expect(result).toEqual(1)
+    })
+})
---
+++ src/romanNumerals.js
@@ -0,0 +1,3 @@
+function romanNumConvert(num){
+    return 1
+}
