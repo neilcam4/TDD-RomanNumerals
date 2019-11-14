c9b7d78f31f7a6e6d6825fbcac30b752820b3bc8
Neil Campbell <neildcampbell@hotmail.com>
2019-11-14 06:02:04 +0000

    added tests for I, V and X and refactored

--- spec/romanNumeralsSpec.js
+++ spec/romanNumeralsSpec.js
@@ -1,7 +1,20 @@
 describe("ROMAN NUMERALS", function(){
+    let num;
+    let result;
+
     it("should return 1 with i", function(){
-        let num = 'I'
-        let result = romanNumConvert(num)
+        num = 'I'
+        result = romanNumConvert(num)
         expect(result).toEqual(1)
+    })
+    it("should return 5 with v", function(){
+        num = 'V'
+        result = romanNumConvert(num)
+        expect(result).toEqual(5)
+    })
+    it("should return 5 with v", function(){
+        num = 'X'
+        result = romanNumConvert(num)
+        expect(result).toEqual(10)
     })
 })
--- src/romanNumerals.js
+++ src/romanNumerals.js
@@ -1,3 +1,11 @@
 function romanNumConvert(num){
-    return 1
-}
+    let numerals = {I:1, V:5, X:10}
+    let newArray = num.split('')
+    let answer;
+    console.log(newArray)
+    newArray.forEach(num => {
+        answer =  numerals[num]
+    })
+    return answer
+    };
+    
